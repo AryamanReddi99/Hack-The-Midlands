6999b7723040d6ed2e5ad150764dbbd5261510af
Chloe <cmlcaron.17@gmail.com>
2019-10-27 10:12:05 +0000

    Updated app for video

--- src/AppWindow.py
+++ src/AppWindow.py
@@ -13,33 +13,6 @@ from BlinkReminder import *
 
 from blink_app import secrets, config, APP_NAME
 
-def make_call():
-    account_sid = secrets['twilio']['account_sid']
-    auth_token = secrets['twilio']['auth_token']
-    from_ = secrets['twilio']['phone_number']
-    to = config['user']['phone_number']
-    client = Client(account_sid, auth_token)
-    x = 0
-
-    while (x < 20):
-        if (x == 1):
-            print('First threshold reached')
-            call = client.calls.create(
-                                    url='https://handler.twilio.com/twiml/EHf78588c67173c8830e80c1845f1ffe87',
-                                    to=to,
-                                    from_=from_
-                                )
-
-        if (x == 10):
-            print('Second threshold reached')
-            call = client.calls.create(
-                                    url='https://handler.twilio.com/twiml/EH7513cf20a7590d433e34dcc02bfa8788',
-                                    to=to,
-                                    from_=from_
-                                )
-        x += 1
-        time.sleep(2)
-
 class AppWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -54,13 +27,6 @@ class AppWindow(QWidget):
         self.imageview = QLabel()
         self.imageview.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
         layout.addWidget(self.imageview)
-
-        # Add manual call button
-        btn_blink = QPushButton('Call!')
-        btn_blink.clicked.connect(make_call)
-        layout.addWidget(btn_blink)
-
-        self.setLayout(layout)
 
         # Create and register ImageRouter on its own QThread
         self.router = ImageRouter()
