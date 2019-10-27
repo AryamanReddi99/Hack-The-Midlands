acff24d4a2b7be6e58a903ed2af2e16fb6458c87
Chloe <cmlcaron.17@gmail.com>
2019-10-27 10:23:19 +0000

    Updated call function

--- src/make_call.py
+++ src/make_call.py
@@ -3,15 +3,14 @@ import time
 
 from blink_app import secrets, config
 
-account_sid = secrets['twilio']['account_sid']
-auth_token = secrets['twilio']['auth_token']
-from_ = secrets['twilio']['phone_number']
-to = config['user']['phone_number']
-client = Client(account_sid, auth_token)
-x = 0
+def make_call(x):
+    account_sid = secrets['twilio']['account_sid']
+    auth_token = secrets['twilio']['auth_token']
+    from_ = secrets['twilio']['phone_number']
+    to = config['user']['phone_number']
+    client = Client(account_sid, auth_token)
 
-while (x < 20):
-    if (x == 1):
+    if (x == 0):
         print('First threshold reached')
         call = client.calls.create(
                                 url='https://handler.twilio.com/twiml/EHf78588c67173c8830e80c1845f1ffe87',
@@ -19,12 +18,11 @@ while (x < 20):
                                 from_=from_
                             )
 
-    if (x == 10):
+    if (x == 1):
         print('Second threshold reached')
         call = client.calls.create(
                                 url='https://handler.twilio.com/twiml/EH7513cf20a7590d433e34dcc02bfa8788',
                                 to=to,
                                 from_=from_
                             )
-    x += 1
-    time.sleep(2)
+    time.sleep(0.5)
